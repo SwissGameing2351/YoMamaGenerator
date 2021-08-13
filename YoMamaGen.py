d3d809ea66a7f62c3edd1e2fe96a64e38705c2af
Scania <74509173+SwissGameing2351@users.noreply.github.com>
2021-08-13 20:25:36 +0200

    Add files via upload

--- YoMamaGen.py
+++ YoMamaGen.py
@@ -1,14 +1,14 @@
-# HELLO YO MAMER FANS !Like yo mama?make sure to like this viedo
+# HELLO YO MAMER FANS !Love yo mama? ? ?????make sure to like this viedo
 import random
 import time
 
-# "Yo Mama so" part (please put very good adjectives in this textfile)
+# "Yo Mama so" part (please put very good adjectives in YoMamaSo.txt)
 with open("YoMamaSo.txt", "r") as file:
     allText = file.read()
     words = allText.split()
     random_word = random.choice(words) 
 
-# the final joke part (please put very good jokes in this textfile again)
+# the final joke part (please put very good jokes in She.txt)
 with open("She.txt", 'r') as file: 
     lines = file.readlines() 
     random_line = random.choice(lines) 
